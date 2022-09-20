0125f7f07ec3aead312c51787f47191b2dbf4758
btkkzc <btkkzc@gmail.com>
2022-09-20 19:28:59 +0200

    Bootstrap package installed, the first version of the form was created

--- src/components/ProductsForm.js
+++ src/components/ProductsForm.js
@@ -1,9 +1,120 @@
-import React from 'react'
+import React from "react";
 
-function ProductsForm() {
+function ProductsForm( ) {
+
+
   return (
-    <div>ProductsForm</div>
-  )
+    <div className=" container bg-warning table-responsive ">
+      <form  >
+        <h2> Add Product </h2>
+        <div>
+          <table >
+            <tbody>
+                <tr >
+                  <th scope="row" className="col-md-1 ">
+                    <div class="input-group input-group-sm mb-3">
+                      <small> Product  </small>
+                    </div>
+                  </th>
+                  <td className="col-md-3 ">
+                    <div class="input-group input-group-sm mb-3">
+                      <span class="input-group-text" id="inputGroup-sizing">
+                        Product Name
+                      </span>
+                      <input
+                        type="text"
+                        class="form-control"
+                        aria-label="Sizing example input"
+                        aria-describedby="inputGroup-sizing-sm"
+                        name = "productname"
+                        data-test="product-form"
+                        required
+                      />
+                    </div>
+                  </td>
+
+                  <td className="col-md-2">
+                    <div class="input-group input-group-sm mb-3">
+                      <span class="input-group-text" id="inputGroup-sizing-sm">
+                        Quantity
+                      </span>
+                      <input
+                        type="number"
+                        class="form-control"
+                        aria-label="Sizing example input"
+                        aria-describedby="inputGroup-sizing-sm"
+                        name = "quantity"
+                        data-test="product-form"
+                        required
+                      />
+                    </div>
+                  </td>
+
+                  <td className="col-md-2">
+                    <div class="input-group input-group-sm mb-3">
+                      <span class="input-group-text" id="inputGroup-sizing-sm">
+                        Type of Product
+                      </span>
+                      <select
+                        className="form-control"
+                        name="productType"
+                        data-test="product-form"
+                        required
+                      >
+                        <option value="other" selected>
+                          Other
+                        </option>
+                        <option value="food"> Food </option>
+                        <option value="book"> Book </option>
+                        <option value="medical product">Medical Product</option>
+                      </select>
+                    </div>
+                  </td>
+
+                  <td className="col-md-2">
+                    <div class="input-group input-group-sm mb-3">
+                      <span class="input-group-text" id="inputGroup-sizing-sm">
+                        Purchasing Type
+                      </span>
+                      <select
+                        name="buyingType"
+                        className="form-control"
+                        data-test="product-form"
+                        required
+                      >
+                        <option value="not Import" selected > not Import </option>
+                        <option value="import"> Import </option>
+                      </select>
+                    </div>
+                  </td>
+
+                  <td className="col-md-2">
+                    <div class="input-group input-group-sm mb-3">
+                      <span class="input-group-text" id="inputGroup-sizing-sm">
+                        Price
+                      </span>
+                      <input
+                        type="number"
+                        class="form-control"
+                        aria-label="Sizing example input"
+                        aria-describedby="inputGroup-sizing-sm"
+                        name = "productPrice"
+                        data-test="product-form"
+                        required
+                      />
+                    </div>
+                  </td>
+                  {/* <td  onClick={deleteProduct} >
+                    x
+                  </td> */}
+                </tr>
+            </tbody>
+          </table>
+        </div>
+        <button type="submit" data-testid="print-button"> Print Receipt </button>
+      </form>
+    </div>
+  );
 }
 
-export default ProductsForm
+export default ProductsForm;
