f3567d7c2995a36f25ae7184f49e0a3e0dd296e9
btkkzc <btkkzc@gmail.com>
2022-09-22 03:31:40 +0200

    css edited

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import './App.css';
-import ProductsForm from './components/ProductsForm/ProductsForm';
-import ReceiptDetails from './components/ReceiptDetails/ReceiptDetails';
 import React, { useState } from "react";
+import ProductsForm from "./components/ProductsForm/ProductsForm";
+import ReceiptDetails from "./components/ReceiptDetails/ReceiptDetails";
 
 
-function App() {
+
+const App = () => {
   const [productList, setproductList] = useState()
   const [listOpen, setlistOpen] = useState(false)
-
+ 
   return (
-    <div className="App">
-      <ProductsForm setproductList = {setproductList} setlistOpen ={setlistOpen} />
-      <ReceiptDetails productList = {productList} listOpen ={listOpen}  />
-    </div>
+      <div className="container-lg text-center">
+        <div className ="row">
+          <ProductsForm setproductList = {setproductList} setlistOpen ={setlistOpen} />
+          <ReceiptDetails productList = {productList} listOpen ={listOpen}  />
+        </div>
+      </div>
   );
-}
+};
 
 export default App;
